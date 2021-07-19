82d210be99c01da04cc02cd76ebca2eb66371091
ggv <m11rrrrr123@gmail.com>
2021-07-19 18:57:18 +0500

    Remove default electron menu

--- src/main.dev.ts
+++ src/main.dev.ts
@@ -79,6 +79,7 @@ const createWindow = async () => {
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true,
+      contextIsolation: false,
     },
   });
 
@@ -86,7 +87,7 @@ const createWindow = async () => {
 
   // @TODO: Use 'ready-to-show' event
   //        https://github.com/electron/electron/blob/master/docs/api/browser-window.md#using-ready-to-show-event
-  mainWindow.webContents.on('did-finish-load', () => {
+  mainWindow.on('ready-to-show', () => {
     if (!mainWindow) {
       throw new Error('"mainWindow" is not defined');
     }
